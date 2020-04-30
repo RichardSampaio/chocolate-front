2f5521e307ca7d25883c19a3bc2215828024ec69
RichardSampaio <richardsampaio90@gmail.com>
2020-04-29 22:40:16 -0300

    upload project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import GlobalStyle from './styles/global';
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+    </div>
+  );
+}
+// function App() {
+// return (
+// <div className="App">
+// <header className="App-header">
+// <p>Chocolate</p>
+// </header>
+// </div>
+// );
+// }
+
+export default App;
